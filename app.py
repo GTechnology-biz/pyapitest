a1f1cc2ea71c11e740bee08043d393936b2e0edd
GTechnology <gtech@gtechnology.biz>
2021-03-24 13:44:22 -0600

    fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 
 url = 'https://icanhazdadjoke.com'
-headers = {'Accept': 'applicati/js'}
+headers = {'Accept': 'application/json'}
 
 
 @app.route('/')
@@ -15,28 +15,28 @@ def index():
 @app.route('/dadjoke')
 def get_joke():
    
-    respse = requests.get(url=url, headers=headers,)
-        data = respse.js()
-            return data
+    response = requests.get(url=url, headers=headers,)
+    data = response.json()
+    return data
 
 
-        @app.route('/dadjoke/search/<string:srch_term>')
-        def joke_search(srch_term):
-                srch_url = url + '/search'
-                    params = dict(
-                                    term = srch_term,
-                                        )
+@app.route('/dadjoke/search/<string:srch_term>')
+def joke_search(srch_term):
+    srch_url = url + '/search'
+    params = dict(
+        term = srch_term,
+    )
 
-                        respse = requests.get(url=srch_url, params=params, headers=headers)
-    data = respse.js()
+    response = requests.get(url=srch_url, params=params, headers=headers)
+    data = response.json()
     return data
 
 @app.route('/dadjoke/j/<string:joke_code>')
 def joke_code(joke_code):
     srch_url = url + '/j/' + joke_code
-    respse = requests.get(url=srch_url, headers=headers)
-        data = respse.js()
-            return data
+    response = requests.get(url=srch_url, headers=headers)
+    data = response.json()
+    return data
 
-        if __name__ == '__main__':
-                app.run(host="0.0.0.0", port=3000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3000)
